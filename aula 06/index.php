50b03667b5e055d2f0e03bf9317823b361e62061
lswolf <lswolf@hotmail.com.br>
2022-06-09 18:29:20 -0300

    autoload

--- aula 06/index.php
+++ aula 06/index.php
@@ -1,11 +1,6 @@
 <?php 
 ini_set('display_errors', 1);
-include 'classes/Usuario.php';
-include 'classes/Cliente.php';
-include 'classes/Gestor.php';
-include 'classes/Produto.php';
-include 'classes/Categoria.php';
-include 'classes/Config/Usuario.php';
+include 'vendor/autoload.php';
 use Classes\Config\Usuario as UsuarioConfig;
 $us1 = new Classes\Usuario();
 $us2 = new UsuarioConfig();
