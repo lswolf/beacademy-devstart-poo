3d423c08ca971ada3386b5a1e5e43b5268c819dc
lswolf <lswolf@hotmail.com.br>
2022-06-09 17:24:57 -0300

    aula 04

---
+++ aula 04/Usuario.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+abstract class Usuario
+{
+    public function __construct(
+    private string $nome,
+    private string $email,
+    private string $senha,
+    private string $cpf
+    ){}
+   
+	function getNome(): string {
+		return $this->nome;
+	}
+	
+
+	function setNome(string $nome): self {
+		$this->nome = $nome;
+		return $this;
+	}
+
+	function getEmail(): string {
+		return $this->email;
+	}
+	
+	
+	function setEmail(string $email): self {
+		$this->email = $email;
+		return $this;
+	}
+	
+	function getSenha(): string {
+		return $this->senha;
+	}
+	
+	
+	function setSenha(string $senha): self {
+		$this->senha = $senha;
+		return $this;
+	}
+
+	function getCpf(): string {
+		return $this->cpf;
+	}
+	
+	
+	function setCpf(string $cpf): self {
+		$this->cpf = $cpf;
+		return $this;
+	}
+}
