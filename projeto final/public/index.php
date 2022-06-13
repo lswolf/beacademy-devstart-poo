f0ec93422d09a174ab6dff969800f03173aed618
lswolf <lswolf@hotmail.com.br>
2022-06-13 14:32:23 -0300

    starting routes

--- projeto final/public/index.php
+++ projeto final/public/index.php
@@ -2,8 +2,50 @@
 include dirname(__DIR__).'/vendor/autoload.php';
 use  App\Controller\IndexController;
 use  App\Controller\ProductController;
-$c = new IndexController();
-$c->indexAction();
-$c->loginAction();
+use  App\Controller\CategoryController;
+use  App\Controller\ErrorController;
+$i = new IndexController();
 $p = new ProductController();
-$p->listAction();
+$c = new CategoryController();
+
+$url = explode('?', $_SERVER['REQUEST_URI'])[0];
+echo $url;
+switch ($url) {
+case  '/':
+    $i->indexAction();
+    break;
+case  '/login':
+    $i->loginAction();
+    break;
+case  '/product':
+    $p->listAction();
+    break;
+case  '/product/add':
+    $p->addAction();
+    break;
+case  '/product/edit':
+    $p->editAction();
+    break;
+case  '/category':
+    $c->listAction();
+    break;
+case  '/category/add':
+    $c->addAction();
+    break;
+case  '/category/edit':
+    $c->editAction();
+    break;
+default:
+  (new ErrorController)->notFoundAction();
+}
+
+$routes = [
+    '/' => [
+        'controller' => IndexController::class,
+        'method' => 'indexAction'
+    ]
+
+
+];
+
+
